79e1f0f4faf1510a99eb6801e0ee8269676c735a
karlZh <2219619335@qq.com>
2017-09-04 11:02:56 +0800

    添加UserManegeController

---
+++ test/unit/posscard/UserManageControllerTests.groovy
@@ -0,0 +1,17 @@
+package posscard
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(UserManageController)
+class UserManageControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
