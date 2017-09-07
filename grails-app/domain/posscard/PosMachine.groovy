7cbb1ecb3b656eb2cc9123f51ce46328b932a824
rong123456789 <printfscanf1>
2017-09-07 15:34:56 +0800

    new3

--- grails-app/domain/posscard/PosMachine.groovy
+++ grails-app/domain/posscard/PosMachine.groovy
@@ -7,11 +7,9 @@ class PosMachine {
     static constraints = {
         supplierId()
         userId()
-<<<<<<< HEAD
-        createTime(display:false)
-=======
+
         createTime(blank:false, nullable: false, display:false)
->>>>>>> cb6f977e8cb707ad9d1e35fee36052ff4f7d32c9
+
     }
     static belongsTo = [supplierId: Supplier]
 }
